35ac9073caee0c4660b1cc7d41f0447f06b1111f
YaChugunov <informed.looser@gmail.com>
2022-06-29 14:42:11 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,53 +3,17 @@ import './style.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Details() {
-  return (
-    <div className="card">
-      <img src="https://i.pravatar.cc/300" className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
-      </div>
-    </div>
-  );
-}
-
-function List() {
-  const [usersList, setUsersList] = useState([]);
+function Details(props) {
   const [userData, setUserData] = useState({});
-  const [selectedUserID, setSelectedUserID] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    fetch(
-      'https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json'
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        console.log('Список пользователей загружен');
-        setUsersList(json);
-      });
-  }, []);
-
-  const selectUserHandle = (userID) => {
-    console.log('Выбран пользователь с ID: ' + userID);
-    setSelectedUserID(userID);
-  };
-
-  useEffect(() => {
-    console.log('ID выбранного пользователя для запроса: ' + selectedUserID);
+    console.log('ID выбранного пользователя для запроса: ' + props.userID);
 
     setIsLoading(true);
     fetch(
       'https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/' +
-        selectedUserID +
+        props.userID +
         '.json'
     ).then((response) => {
       if (response.status !== 200) {
@@ -66,11 +30,40 @@ function List() {
         setIsLoading(false);
       });
     });
-  }, [selectedUserID]);
+  }, [props.userID]);
 
   return (
+    <div className="card">
+      <img src="https://i.pravatar.cc/300" className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{userData.name}</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
+}
+
+function List(props) {
+  const [selectedUserID, setSelectedUserID] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const selectUserHandle = (userID) => {
+    console.log('Выбран пользователь с ID: ' + userID);
+    setSelectedUserID(userID);
+  };
+
+  return (
+    <div className="row">
+    <h1 className="display-4 mb-4">List and details</h1>
+        <div className="col">
     <ul className="list-group">
-      {usersList.map((user) => (
+      {props.usersList.map((user) => (
         <li
           className="list-group-item"
           key={user.id}
@@ -80,21 +73,34 @@ function List() {
         </li>
       ))}
     </ul>
+    </div>
+    <div className="col">
+          <Details userID={} />
+        </div>
+        </div>
+
+
   );
 }
 
 export default function App() {
+  const [usersList, setUsersList] = useState([]);
+  const [userID, setUserID] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      'https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json'
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        console.log('Список пользователей загружен');
+        setUsersList(json);
+      });
+  }, []);
+
   return (
     <div className="container">
-      <div className="row">
-        <h1 className="display-4 mb-4">List and details</h1>
-        <div className="col">
-          <List />
-        </div>
-        <div className="col">
-          <Details />
-        </div>
-      </div>
+          <List usersList={usersList} />
     </div>
   );
 }
